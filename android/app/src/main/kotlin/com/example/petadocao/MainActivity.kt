a6e5cbad6453f1ed18ffe5a5423f48f86e32377c
JoaoVictorPires <joaovictorsantos0213@gmail.com>
2025-04-07 19:25:24 -0300

    feat: implement user registration and login functionality

--- android/app/src/main/kotlin/com/example/petadocao/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.petadocao
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/models/pet.dart
+++
@@ -1,8 +0,0 @@
-// lib/models/pet.dart
-class Pet {
-  final String nome;
-  final String idade;
-  final String imagem;
-
-  const Pet({required this.nome, required this.idade, required this.imagem});
-}
